2dde751b0f354031922cfc33df5eb134bd07fb07
neonicoo <nicolas.mendiboure@insa-lyon.fr>
2020-12-21 20:38:10 +0100

    Création d'un objet Yoshi, test des foonctions set_speed(), set_max_speed(), Accelerate() et WhatAmI() dans le main.cpp

--- main.cpp
+++ main.cpp
@@ -15,5 +15,16 @@ int main(){
     mario.Accelerate();
     std::cout << "Vitesse mario apres acceleration  : " << mario.speed() << std::endl ;
     
+
+    Yoshi yoshi(8.1,40.0);
+    std::cout << yoshi.WhatAmI() << std::endl;
+
+    yoshi.set_speed(7.5);
+    yoshi.set_max_speed(40.5);
+    std::cout << "Vitesse yoshi : " << yoshi.speed() << "  max : " << yoshi.max_speed() << std::endl ;
+    yoshi.Accelerate();
+    std::cout << "Vitesse yoshi apres acceleration : " << yoshi.speed()  << std::endl ;
+    
+    
     return 0 ;
 }
