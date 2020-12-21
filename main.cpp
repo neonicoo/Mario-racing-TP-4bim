f84bfae9e9e4a93b8966d8575677a7e1d9fd8e84
neonicoo <nicolas.mendiboure@insa-lyon.fr>
2020-12-21 20:24:00 +0100

    Création d'un objet Mario, test fonction Accelerate, test fonction WhatAmI dans le main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <cstdlib>
 #include <vector>
-#include "Character.h"
+#include "Mario.h"
 
 int main(){
 
     Character perso (5.0,10.0);
     std::cout << perso.WhatAmI() << std::endl;
 
+    Mario mario(7.0,25.0);
+    std::cout << mario.WhatAmI() << std::endl;
+    std::cout << "Vitesse mario : " << mario.speed() << std::endl ;
+
+    mario.Accelerate();
+    std::cout << "Vitesse mario apres acceleration  : " << mario.speed() << std::endl ;
+    
     return 0 ;
 }
