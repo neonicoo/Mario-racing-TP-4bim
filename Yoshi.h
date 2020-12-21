736665fc08f3718b888311926a308804306ec2ae
neonicoo <nicolas.mendiboure@insa-lyon.fr>
2020-12-21 20:26:50 +0100

    Création de la classe fille Yoshi issus de Character : - Attribut : crests_ (nb de piques) - Constructeur par défaut - Constructeur faisant appel à la classe mère (2 arguments) - Methode accelerate - Methode WhatAmI - Getter crests - Setter crests

--- Yoshi.h
+++ Yoshi.h
@@ -1,1 +1,22 @@
 #include "Character.h"
+
+class Yoshi : public Character{
+
+    protected:
+        int crests_;
+
+    public:
+        Yoshi();
+        Yoshi(float speed, float max_speed);
+        void set_crests(int new_crests);
+
+        virtual std::string WhatAmI() const override;
+        virtual void Accelerate() override ;
+    
+        inline int crests() const{
+            return this->crests_;
+        }
+
+        
+        ~Yoshi() = default;
+};
